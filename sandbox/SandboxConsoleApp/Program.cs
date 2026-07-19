6850e2527b809408d57b20d6eb156db10d061e22
Proudust <proudust@gmail.com>
2026-07-20 02:19:39 +0900

    :white_check_mark: Fix SandboxConsoleApp to use WebSerializer.AOT

--- sandbox/SandboxConsoleApp/Program.cs
+++ sandbox/SandboxConsoleApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.Serialization;
 using Cysharp.Web;
+using Cysharp.Web.Providers;
 using Proudust.Web;
+
+var options = WebSerializerOptions.Default with
+{
+    Provider = WebSerializerProvider.Create([
+        PrimitiveWebSerializerProvider.Instance,
+        BuiltinWebSerializerProvider.Instance,
+        AttributeWebSerializerProvider.Instance,
+        GenericsWebSerializerProvider.Instance,
+        CollectionWebSerializerProvider.Instance,
+        ObjectFallbackWebSerializerProvider.Instance,
+        // ObjectGraphWebSerializerProvider.Instance, // Does not work in AOT due to reflection.
+        GenerateWebSerializerProvider.Instance,
+    ]),
+};
 
 Console.WriteLine(WebSerializer.ToQueryString("https://www.google.co.jp/search", new UrlParams
 {
     Query = "hello",
-}));
+}, options));
 
 readonly partial struct UrlParams
 {
